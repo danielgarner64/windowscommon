657ef37b20504b4edf691d6060c15c28fb2072df
danielgarner64 <44664284+danielgarner64@users.noreply.github.com>
2022-05-03 13:31:12 +1000

    Create ConfigureWindows.ps1

---
+++ ConfigureWindows.ps1
@@ -0,0 +1,24 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Install-WMF3Hotfix.ps1"
+$file = "$env:temp\Install-WMF3Hotfix.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -Verbose
+
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP -ForceNewSSLCert -CertValidityDays 3650
+
+
+powershell.exe /c "powercfg /change monitor-timeout-ac 0"
+powershell.exe /c "powercfg /change standby-timeout-ac 0"
+powershell.exe /c "sc.exe config WinRM start= auto"
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+$reg_winlogon_path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
+Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
